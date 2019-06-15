cddd0badaa4ef6dc9f2c8d9f63f1d8c67bf0aca8
Nate Roling <nateroling@gmail.com>
2019-06-15 13:52:08 -0500

    reactToKnockout components can be registered and rendered

--- src/react-knockout.spec.tsx
+++ src/react-knockout.spec.tsx
@@ -4,29 +4,70 @@ import * as ko from "knockout";
 
 const EmptyDivComponent = (props: unknown) => <div />;
 
+let root: Element = null;
+
+beforeEach(() => {
+  document.body.innerHTML = "";
+  root = document.createElement("main");
+  document.body.append(root);
+});
+
+afterEach(() => {
+  ko.cleanNode(root);
+});
+
 test("reactToKnockout returns a valid Knockout component config", () => {
   const result = reactToKnockout(EmptyDivComponent);
   expect(result).toHaveProperty("template");
   expect(typeof result.template).toBe("string");
-  expect(result).toHaveProperty("createViewModel");
-  expect(result.createViewModel).toBeInstanceOf(Function);
+  expect(result).toHaveProperty("viewModel");
+  expect(result.viewModel).toHaveProperty("createViewModel");
+  //expect(result.viewModel.createViewModel).toBeInstanceOf(Function);
 });
 
 test("reactToKnockout creates a valid viewModel", () => {
   const params: unknown = null;
-  const componentInfo: unknown = null;
+  const componentInfo: KnockoutComponentTypes.ComponentInfo = null;
   const config = reactToKnockout(EmptyDivComponent);
-  const viewModel = config.createViewModel(params, componentInfo);
+
+  const viewModel = (config.viewModel as KnockoutComponentTypes.ViewModelFactoryFunction).createViewModel(
+    params,
+    componentInfo
+  );
 
   expect(viewModel).toBeInstanceOf(Object);
 });
 
-test("reactToKnockout components can be render via react binding", () => {
-  document.body.innerHTML = `<div data-bind="{ react: { component: component }}">`;
-  expect(document.body.firstElementChild.tagName).toBe("DIV");
-  expect(document.body.firstElementChild.childElementCount).toBe(0);
+test("reactToKnockout components can be rendered via react binding", () => {
+  const component = () => <div>SUCCESS</div>;
+  root.innerHTML = `<div data-bind="{ react: { component: component }}">`;
+  expect(root.firstElementChild.tagName).toBe("DIV");
+  expect(root.firstElementChild.childElementCount).toBe(0);
 
-  ko.applyBindings({ component: EmptyDivComponent }, document.body);
-  expect(document.body.firstElementChild.tagName).toBe("DIV");
-  expect(document.body.firstElementChild.childElementCount).toBe(1);
+  ko.applyBindings({ component: component }, root);
+  expect(root.firstElementChild.tagName).toBe("DIV");
+  expect(root.firstElementChild.childElementCount).toBe(1);
+  expect(root.firstElementChild.firstElementChild.textContent).toBe("SUCCESS");
+});
+
+test("reactToKnockout components can be registered and rendered", done => {
+  const component = () => <div>SUCCESS</div>;
+  const config = reactToKnockout(component);
+  ko.components.register("test-component", config);
+
+  root.innerHTML = `<test-component></test-component>`;
+  expect(root.firstElementChild.tagName).toBe("TEST-COMPONENT");
+  expect(root.firstElementChild.childElementCount).toBe(0);
+
+  ko.applyBindings({}, root);
+
+  // TODO Find another way to wait for KO to apply component bindings.
+  setTimeout(() => {
+    expect(root.firstElementChild.tagName).toBe("TEST-COMPONENT");
+    expect(root.firstElementChild.childElementCount).toBe(1);
+    expect(root.firstElementChild.firstElementChild.textContent).toBe(
+      "SUCCESS"
+    );
+    done();
+  }, 100);
 });
--- src/react-knockout.tsx
+++ src/react-knockout.tsx
@@ -5,25 +5,40 @@ import { ReactComponentLike } from "prop-types";
 
 export const reactToKnockout = (
   reactComponent: ReactComponentLike
-): KnockoutComponentTypes.ComponentConfig => {
-  return {
-    template: "<div>awesome sauce</div>",
-    createViewModel: () => {
-      return { component: reactComponent };
+): KnockoutComponentTypes.ComponentConfig => ({
+  viewModel: {
+    createViewModel: function(params, componentInfo) {
+      //const props = params;
+      //const bindingContext = ko.contextFor(componentInfo.element);
+
+      /*
+      props.children = (
+        <KnockoutComponent
+          bindingContext={bindingContext}
+          templateNodes={componentInfo.templateNodes}
+        />
+      );
+      */
+
+      const viewModel = {
+        reactOptions: {
+          component: reactComponent,
+          props: params
+        }
+      };
+      return viewModel;
     }
-  };
-};
+  },
+  template: "<div data-bind='react: reactOptions'></div>"
+});
 
 // Copied from https://github.com/calvinwoo/knockout-bind-react
 ko.bindingHandlers.react = {
   init: function(element) {
-    console.log("INIT");
-
     ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
       ReactDOM.unmountComponentAtNode(element);
     });
 
-    console.log("INIT2");
     return {
       controlsDescendantBindings: true
     };
@@ -31,6 +46,7 @@ ko.bindingHandlers.react = {
 
   update: function(element, valueAccessor, allBindings) {
     var options = ko.unwrap(valueAccessor());
+    console.log("UPDATE");
 
     if (options && options.component) {
       var componentInstance = React.createElement(
