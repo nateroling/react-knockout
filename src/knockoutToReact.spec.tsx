afd0e7f54fad491a96fde7cbfee01b6520064869
Nate Roling <nateroling@gmail.com>
2019-06-23 10:22:24 -0500

    Add a failing test for updating KO child nodes.

--- src/knockoutToReact.spec.tsx
+++ src/knockoutToReact.spec.tsx
@@ -67,6 +67,39 @@ test("knockoutToReact binds a named component with componentTemplateNodes", done
   }, 100);
 });
 
+test("knockoutToReact updates a named component with componentTemplateNodes", done => {
+  const config = {
+    viewModel: { createViewModel: () => ({ content: "SUCCESS" }) },
+    template: `<span data-bind="template: { nodes: $componentTemplateNodes }"></span>`
+  };
+  registerComponent("test-component", config);
+  const ParentComponent = knockoutToReact("test-component");
+  const ChildComponent = (props: { value: string }) => <div>{props.value}</div>;
+  let message = "INITIAL";
+
+  ReactDOM.render(
+    <ParentComponent>
+      <ChildComponent value={message}></ChildComponent>
+    </ParentComponent>,
+    root
+  );
+  setTimeout(() => {
+    expect(root.innerHTML).toContain("INITIAL");
+
+    message = "SUCCESS";
+    ReactDOM.render(
+      <ParentComponent>
+        <ChildComponent value={message}></ChildComponent>
+      </ParentComponent>,
+      root
+    );
+    setTimeout(() => {
+      expect(root.innerHTML).toContain("SUCCESS");
+      done();
+    }, 100);
+  }, 100);
+});
+
 test("knockoutToReact ignores non-observable params", done => {
   const config = {
     viewModel: {
